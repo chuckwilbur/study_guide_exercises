a23d6d9ad09bf501e4fb253e5ba3078c03c95415
Chuck Wilbur <github@chuckwilbur.com>
2020-12-16 13:17:36 -0600

    Remove direct stack invocation

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ from study_guide_exercises.study_guide_exercises_stack import StudyGuideExercise
 from study_guide_exercises.apppipeline_stack import AppPipelineStack
 
 app = core.App()
-StudyGuideExercisesStack(app, "study-guide-exercises", env={
-    'account': '441875730569',
-    'region': 'us-east-2'
-})
 AppPipelineStack(app, "AppPipelineStack", env={
     'account': '441875730569',
     'region': 'us-east-2'
