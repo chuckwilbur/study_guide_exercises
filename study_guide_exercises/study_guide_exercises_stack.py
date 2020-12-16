86b12b30ce24eff9266c66bd8574fe0e00f4ca43
Chuck Wilbur <github@chuckwilbur.com>
2020-12-16 15:46:36 -0600

    Add web server instance

--- study_guide_exercises/study_guide_exercises_stack.py
+++ study_guide_exercises/study_guide_exercises_stack.py
@@ -24,9 +24,42 @@ class StudyGuideExercisesStack(core.Stack):
         self.public_subnet_id = vpc.public_subnets[0].subnet_id
         self.private_subnet_id = vpc.private_subnets[0].subnet_id
 
+        # role for the ec2 instance to assume
         role = iam.Role(self, 'devassoc-webserver',
                         assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
                         role_name='devassoc-webserver')
         core.Tags.of(role).add('project', 'devassoc')
         role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('AmazonPollyReadOnlyAccess'))
         role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('TranslateReadOnly'))
+
+        # security group for the ec2 instance
+        security_group = ec2.SecurityGroup(self, 'devassoc-sg',
+                                           vpc=vpc,
+                                           security_group_name='restricted-http-ssh',
+                                           description='HTTP and SSH from my IP address only')
+        security_group.add_ingress_rule(
+            ec2.Peer.ipv4('99.116.136.249/32'), ec2.Port.tcp(22), 'SSH from my IP')
+        security_group.add_ingress_rule(
+            ec2.Peer.ipv4('99.116.136.249/32'), ec2.Port.tcp(80), 'HTTP from my IP')
+
+        # user data for the ec2 instance
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            'yum install httpd -y',
+            'systemctl start httpd',
+            'systemctl enable httpd'
+        )
+
+        # the ec2 instance
+        instance = ec2.Instance(self, 'webserver-ec2',
+                                instance_name='webserver',
+                                instance_type=ec2.InstanceType('t2.micro'),
+                                machine_image=ec2.MachineImage.generic_linux(
+                                    ami_map={'us-east-2': 'ami-09558250a3419e7d0'}
+                                ),
+                                vpc=vpc,
+                                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+                                role=role,
+                                user_data=user_data,
+                                security_group=security_group,
+                                key_name='devassoc')
